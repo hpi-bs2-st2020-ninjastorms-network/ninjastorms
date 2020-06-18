c7550291e7f8b81c047ed9c213663ac1265dbc91
Lorenz Woth <lorenz.woth@outlook.com>
2020-06-18 14:39:12 +0200

    Added ARP and ethernet headers

---
+++ kernel/network/arp.h
@@ -0,0 +1,21 @@
+#include <sys/types.h>
+#include "ethernet_frame.h" 
+
+// OPCODES
+#define ARP_REQUEST = 0x0001;
+#define ARP_RESPONSE = 0x0002;
+
+// https://wiki.osdev.org/ARP
+struct arp {
+    uint16_t hardware_type; 
+    uint16_t protocol_type;
+    uint16_t opcode; // ARP OP Code: see above
+
+    uint8_t src_mac_addr[6];
+    uint8_t dest_mac_addr[6];
+
+    uint32_t src_ip_address[4];
+    uint32_t dest_ip_address[4];
+};
+
+void process_arp(const ethernet_frame* frame);
