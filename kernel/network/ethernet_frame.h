150b34f1442a46b86333942bb9ae85034c0d877d
Lorenz Woth <lorenz.woth@outlook.com>
2020-06-18 14:39:12 +0200

    Added ARP and ethernet headers

---
+++ kernel/network/ethernet_frame.h
@@ -0,0 +1,13 @@
+#include <sys/types.h>
+
+typedef enum {
+    ARP = 0x0806,
+    IPv4 = 0x0800
+} ether_type;
+
+struct ethernet_frame {
+    uint8_t source_mac [6];
+    uint8_t dest_mac [6];
+    ether_type ether_type;
+    uint8_t payload[8];
+};
