301d43225afb94e79d6657bf8fc9591ec5e25b31
Lorenz Woth <lorenz.woth@outlook.com>
2020-07-13 20:05:40 +0200

    Add ethernet frame files and rework mac read
    Co-authored-by: sirkrypt0 <tobiaskantusch@online.de>
    Co-authored-by: Felix Auringer <48409110+felixauringer@users.noreply.github.com>

--- kernel/network/ethernet_frame.c
+++ kernel/network/ethernet_frame.c
@@ -18,24 +18,20 @@
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
  ******************************************************************************/
 
-#pragma once
-
+#include "ethernet_frame.h"
 #include <sys/types.h>
+#include <stdio.h>
 
-typedef enum {
-  ARP = 0x0806,
-  IPv4 = 0x0800,
-  IPv6 = 0x86dd
-} ether_type;
+uint64_t
+get_source_mac(ethernet_frame_t *frame) 
+{
+  uint64_t source_mac = *(uint64_t*)frame->source_mac;
+  return source_mac & 0x0000FFFFFFFFFFFF;
+}
 
-struct __ethernet_frame {
-  volatile uint8_t dest_mac [6];
-  volatile uint8_t source_mac [6];
-  volatile ether_type ether_type;
-  volatile uint8_t payload[];
-  // volatile uint8_t crc[4];
-} __attribute__((packed));
-typedef struct __ethernet_frame ethernet_frame_t;
-
-uint64_t get_source_mac (ethernet_frame_t *frame);
-uint64_t get_dest_mac (ethernet_frame_t *frame);
+uint64_t
+get_dest_mac(ethernet_frame_t *frame) 
+{
+  uint64_t dest_mac = *(uint64_t*)frame->dest_mac;
+  return dest_mac & 0x0000FFFFFFFFFFFF;
+}
