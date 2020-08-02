1d7449d388deb4dc8310acc9c83667f7c636a193
sirkrypt0 <tobiaskantusch@online.de>
2020-08-02 12:34:54 +0200

    Add basic linter script

---
+++ .indent.pro
@@ -0,0 +1,9 @@
+-gnu
+//no space after function call
+-npcs
+// please use 2 spaces instead of tabs
+-nut
+-i2
+// honour existing newlines
+-hnl
+-ppi0
---
+++ test/lint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# for now only include our source files to avoid huge merge conflicts
+include_files=(\
+	"./kernel/logger/*.c" "./kernel/logger/*.h" \
+	"./kernel/network/*.c" "./kernel/network/*.h" \
+	"./kernel/pci/*.c" "./kernel/pci/*.h" \
+	"./kernel/time.c" "./kernel/pci/time.h" \
+	"./libc/unistd/*.c" "./libc/include/unistd.h" \
+)
+
+status=0
+files=$(printf ' -o -path "%s"' "${include_files[@]}")
+while read file; do
+    indent $file -o "$file.indented.c"
+    output=$(diff -u --color='auto' $file "$file.indented.c")
+    # if files differ set status and print output
+    if [[ -n "$output" ]]; then
+    	status=1
+    	echo "$output"
+    fi
+    rm "$file.indented.c"
+done < <(eval "find . -type f \( -path "" "$files" \) -printf '%h\0%d\0%p\n'" | sort -t '\0' -n | awk -F '\0' '{print $3}')
+
+exit $status
